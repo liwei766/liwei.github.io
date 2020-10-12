b8eb1c4ac1ddd33d47bb5baff745631c260de9cd
Liwei <uccblw@gmail.com>
2020-10-12 13:00:14 +0900

    apigateway lambda dynamodb -- serverless contact form

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -6,63 +6,6 @@ $(function () {
         submitError: function ($form, event, errors) {
             // additional error messages or events
         },
-        submitSuccess: e => {
-            e.preventDefault();
-
-            const form = document.querySelector('form');
-            const submitResponse = document.querySelector('#success');
-            const formURL = 'https://www.smart-infra.net/Prod/submitForm';
-    
-            // Capture the form data
-            let data = {};
-            Array.from(form).map(input => (data[input.id] = input.value));
-            console.log('Sending: ', JSON.stringify(data));
-            submitResponse.innerHTML = 'Sending...'
-    
-            // Create the AJAX request
-            var xhr = new XMLHttpRequest();
-            xhr.open(form.method, formURL, true);
-            xhr.setRequestHeader('Accept', 'application/json; charset=utf-8');
-            xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    
-            // Send the collected data as JSON
-            xhr.send(JSON.stringify(data));
-    
-            xhr.onloadend = response => {
-                if (response.target.status === 200) {
-                    $("#success").html("<div class='alert alert-success'>");
-                    $("#success > .alert-success")
-                        .html(
-                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-                        )
-                        .append("</button>");
-                    $("#success > .alert-success").append(
-                        "<strong>Your message has been sent. </strong>"
-                    );
-                    $("#success > .alert-success").append("</div>");
-                    //clear all fields
-                    $("#contactForm").trigger("reset");
-                } else {
-                    $("#success").html("<div class='alert alert-danger'>");
-                    $("#success > .alert-danger")
-                        .html(
-                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-                        )
-                        .append("</button>");
-                    $("#success > .alert-danger").append(
-                        $("<strong>").text(
-                            "Sorry " +
-                                firstName +
-                                ", it seems that my mail server is not responding. Please try again later!"
-                        )
-                    );
-                    $("#success > .alert-danger").append("</div>");
-                    //clear all fields
-                    $("#contactForm").trigger("reset");
-                }
-            };    
-        },
-        /***
         submitSuccess: function ($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
@@ -78,7 +21,7 @@ $(function () {
             $this = $("#sendMessageButton");
             $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
             $.ajax({
-                url: "https://hvu3sq7v2j.execute-api.ap-northeast-1.amazonaws.com/Prod/submitForm",
+                url: "https://www.smart-infra.net/Prod/submitForm",
                 type: "POST",
                 data: {
                     name: name,
@@ -128,7 +71,6 @@ $(function () {
                 },
             });
         },
-        *****/
         filter: function () {
             return $(this).is(":visible");
         },
