c91f65746640df5f075bc788e740a2ff76ec2c5e
Liwei <uccblw@gmail.com>
2020-10-12 21:27:07 +0900

    apigateway lambda dynamodb -- serverless contact form

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -1,3 +1,4 @@
+/***
 $(function () {
     $(
         "#contactForm input,#contactForm textarea,#contactForm button"
@@ -21,7 +22,7 @@ $(function () {
             $this = $("#sendMessageButton");
             $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
             $.ajax({
-                url: "https://www.smart-infra.net/Prod/submitForm",
+                url: "/Prod/submitForm",
                 type: "POST",
                 data: {
                     name: name,
@@ -31,7 +32,6 @@ $(function () {
                 },
                 crossDomain: true,
                 contentType: 'application/json',
-                dataType: "json",
                 cache: false,
                 success: function () {
                     // Success message
@@ -84,8 +84,72 @@ $(function () {
         $(this).tab("show");
     });
 });
+***/
+
+$(function () { $("#contactForm input,#contactForm textarea,#contactForm button").not("[type=submit]").jqBootstrapValidation(); } );
 
 /*When clicking on Full hide fail/success boxes */
 $("#name").focus(function () {
     $("#success").html("");
 });
+
+
+(() => {
+    const form = document.querySelector('form');
+    const submitResponse = document.querySelector('#success');
+    const formURL = 'https://www.smart-infra.net/Prod/submitForm';
+
+    form.onsubmit = e => {
+      e.preventDefault();
+
+      // Capture the form data
+      let data = {};
+      Array.from(form).map(input => (data[input.id] = input.value));
+      console.log('Sending: ', JSON.stringify(data));
+      submitResponse.innerHTML = 'Sending...'
+
+      // Create the AJAX request
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', formURL, true);
+      xhr.setRequestHeader('Accept', 'application/json; charset=utf-8');
+      xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+
+      // Send the collected data as JSON
+      xhr.send(JSON.stringify(data));
+
+      xhr.onloadend = response => {
+        if (response.target.status === 200) {
+            // Success message
+            $("#success").html("<div class='alert alert-success'>");
+            $("#success > .alert-success")
+                .html(
+                    "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                )
+                .append("</button>");
+            $("#success > .alert-success").append(
+                "<strong>Your message has been sent. </strong>"
+            );
+            $("#success > .alert-success").append("</div>");
+            //clear all fields
+            $("#contactForm").trigger("reset");
+        } else {
+            // Fail message
+            $("#success").html("<div class='alert alert-danger'>");
+            $("#success > .alert-danger")
+                .html(
+                    "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                )
+                .append("</button>");
+            $("#success > .alert-danger").append(
+                $("<strong>").text(
+                    "Sorry " + $("input#name").val() +
+                        ", it seems that my mail server is not responding. Please try again later!"
+                )
+            );
+            $("#success > .alert-danger").append("</div>");
+            //clear all fields
+            $("#contactForm").trigger("reset");
+        }
+      };    
+    };
+  })();
